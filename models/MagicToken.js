a55e5b2fa42bc332eb40430d47ab6064f6e0e63a
Jobswitchco <jobswitchco@gmail.com>
2026-02-16 22:55:40 +0530

    Date Feb 16 v1.0

--- models/MagicToken.js
+++ models/MagicToken.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 const MagicTokenSchema = new Schema({
   token: { type: String, required: true, unique: true },
   user_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  chatUsername: { type: String, default: null },
   used: { type: Boolean, default: false },
   created_at: { type: Date, default: Date.now },
   expires_at: { type: Date, required: true },
